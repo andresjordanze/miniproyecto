bdd482863ca2a41730a5c2e41448e1f8721c0aa2
andresjordanze <andresjordanze@gmail.com>
2014-02-19 11:16:08 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miniproyecto::Application.config.secret_key_base = '94b474e0e3582ebe9aa49952ed9cbcd96d083bf91be365704cacc244e9274a1c73809d7e191098d2df09dad15c603318fb7596794c0461823bfd57ce232cf6d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Miniproyecto::Application.config.session_store :cookie_store, key: '_miniproyecto_session'
